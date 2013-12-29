39274f2b8bb650431458167b047477ff84d09a41
Andreas <andreas@nowhere.local>
2013-12-29 14:17:29 +0100

    setup new path for testing

--- test/javafxapplication1/business/boundary/NoteServiceTest.java
+++ test/javafxapplication1/business/boundary/NoteServiceTest.java
@@ -22,7 +22,7 @@ public class NoteServiceTest {
     @Test
     public void testListImpl() {
     
-        NoteService s = new NoteServiceImpl("f:/tmp/MySecretNoteStorage_A/");
+        NoteService s = new NoteServiceImpl("f:/tmp/MySecretNoteStorage_DEBUG/");
      
         for (Iterator<NoteEntity> iter = s.list().listIterator(); iter.hasNext();) {
             NoteEntity note = iter.next();
